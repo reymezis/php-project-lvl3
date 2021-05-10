536da6bc686ad93a274b04406fb90efbbb9d0285
Reymezis <qq1s@bk.ru>
2021-05-10 14:26:41 +0600

    add urls routes and actions

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UrlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $urls = DB::table('urls')->get();
+        return view('url.index', ['urls' => $urls]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $url = parse_url($request->input('url.name'));
+        $messages = [
+            'required' => 'Поле не должно быть пустым.',
+            'url' => 'Введён некорректный адрес сайта.',
+            'unique' => 'Данный адрес уже добавлен.'
+        ];
+        $this->validate($request, [
+            'url.name' => 'required|url|unique:urls,name',
+        ], $messages);
+        $scheme   = isset($url['scheme']) ? mb_strtolower($url['scheme']) . '://' : '';
+        $host     = isset($url['host']) ? mb_strtolower($url['host']) : '';
+        if ($scheme && $host) {
+            session()->flash('status', 'Сайт добавлен!');
+            $url = DB::table('urls')->insert(
+                [
+                    'name' => "$scheme$host",
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]
+            );
+        }
+
+        return redirect()->route('urls.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $url = DB::table('urls')->find($id);
+        if (!$url) {
+            abort(404);
+        }
+        return view('url.show', ['url' => $url]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::resource('urls', UrlController::class);
